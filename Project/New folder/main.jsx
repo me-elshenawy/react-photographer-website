33aaa29b815bc063ab94b17f2077b6eac00d806b
mahmoud elshenawy <me.20031128@gmail.com>
2025-09-28 14:26:16 +0300

    Delete Project/New folder directory

--- Project/New folder/main.jsx
+++
@@ -1,6 +0,0 @@
-import { createRoot } from "react-dom/client";
-import Main_Layout from "./Layout/Main_Layout";
-
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
-createRoot(document.getElementById("root")).render(<Main_Layout />);
